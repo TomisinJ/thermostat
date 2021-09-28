56d8887ed35d6b30d5bdbc07d611ae6712a51b36
Keldra <KeldraSJ@gmail.com>
2021-09-28 17:13:52 +0100

    adding up and down function

--- jasmine-standalone-3.9.0/spec/thermostatSpec.js
+++ jasmine-standalone-3.9.0/spec/thermostatSpec.js
@@ -8,4 +8,19 @@ describe('Thermostat', () => {
   it('starts with a temperature of 20', () => {
     expect(thermostat.temperature).toEqual(20);
   });
+
+  describe('Up Function', () => {
+
+    it('increases the temperature by 1', () => {
+      thermostat.up(1) 
+      expect(thermostat.temperature).toEqual(21);
+    });
+  });
+
+  describe('Down Function', () => {
+    it('decreases the temperature by 1', () => {
+      thermostat.down(1)
+      expect(thermostat.temperature).toEqual(19);
+    });
+  });
 });
--- jasmine-standalone-3.9.0/src/thermostat.js
+++ jasmine-standalone-3.9.0/src/thermostat.js
@@ -2,4 +2,16 @@ class Thermostat {
   constructor(temperature) {
     this.temperature = 20;
   }
+  up(num) {
+    return this.temperature += num 
+  }
+
+  down(num) {
+    return this.temperature -= num
+  }
 }
+
+let thermostat = new Thermostat
+
+// console.log(thermostat.up(4))
+console.log(thermostat.down(4))
